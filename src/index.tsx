35aecfcd9d45b25ed07709ce9b09f8e9cd536e76
Kyle Pfromer <kpfromer2@gmail.com>
2019-05-01 18:52:13 -0600

    Update index to provide Stores

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import config from './config';
+import { Provider } from 'mobx-react';
+// Stores
+import baseComponentStore from './stores/BaseComponent.store';
+import generatedComponentStore from './stores/GeneratedComponent.store';
+import componentStructureStore from './stores/ComponentStructure.store';
+
+export const store = () => ({
+  baseComponentStore,
+  generatedComponentStore,
+  componentStructureStore
+})
 
 ReactDOM.render(
-  <App
-    baseComponents={config.baseInputs}
-    componentStructures={config.componentStructures} 
-    generateDefaultValue={config.generateDefaultValue}
-  />, document.getElementById('root'));
+  <Provider {...store()}>
+    <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
