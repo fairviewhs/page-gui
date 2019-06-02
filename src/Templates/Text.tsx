1a47c3903d7109182ec61b5fe0c697dac4857843
Kyle Pfromer <kpfromer2@gmail.com>
2019-06-01 18:40:09 -0600

    Update components imports

--- src/Templates/Text.tsx
+++ src/Templates/Text.tsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { EditorState, Editor } from 'draft-js';
 
 export interface TextProps{
-  text: string;
+  text: EditorState;
 }
 
 export default class Text extends Component<TextProps, any> {
   public render() {
     return (
       <p className="paddedText">
-        {this.props.text}
+        <Editor readOnly editorState={this.props.text} onChange={() => {}} />
       </p>
     );
   }
